e4ca7582cfa74296efe135f26941de14be98aa2a
SamuelDesN <vazquezsamuel877@gmail.com>
2025-01-17 20:14:18 +0100

    renamed:    app.js -> api/app.js 	renamed:    middlewares.js -> api/middlewares.js 	deleted:    index.js

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const middlewares = require('./middlewares');
-const api = require('./api');
+const api = require('.');
 const { parse } = require('dotenv');
 
 const app = express();
