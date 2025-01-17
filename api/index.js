8100c726bb3d7b14238996295ff2a8d1a8578b88
SamuelDesN <vazquezsamuel877@gmail.com>
2025-01-17 20:40:04 +0100

    modified:   api/index.js 	modified:   package-lock.json 	modified:   package.json

--- api/index.js
+++ api/index.js
@@ -1,8 +1,4 @@
-const express = require("express");
-const app = express();
+const app = require('./app');
+const serverless = require('serverless-http');
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
+module.exports = serverless(app);
