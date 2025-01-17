db296749acba5c921428f0b8a8a2626d222ff2a5
SamuelDesN <vazquezsamuel877@gmail.com>
2025-01-17 19:43:41 +0100

    modified:   test/api.test.js

--- test/api.test.js
+++ test/api.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 
-const app = require('../src/app');
+const app = require('./app');
 
 describe('GET /api/v1', () => {
   it('responds with a json message', (done) => {
