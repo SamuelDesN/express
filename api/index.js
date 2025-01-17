0c04d8d5515de97384492b1281d07b1e6af9e133
SamuelDesN <vazquezsamuel877@gmail.com>
2025-01-17 21:04:10 +0100

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const app = express();
+const app = require('./app');
+const serverless = require('serverless-http');
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+module.exports = serverless(app);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
